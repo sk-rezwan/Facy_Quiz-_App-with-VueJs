1066d278587a440540b5495a67bf4a2ea5303633
sk-rezwan <skrezwan12@gmail.com>
2019-09-28 21:40:49 +0600

    header submit bun inc dec

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app">
-    <Header/>
+    <Header
+    :numCorrect = "numCorrect"
+    :numTotal = "numTotal"
+    />
     <b-container class="col-sm-6 offset-3">
       <b-row>
-        <b-col><QuestionBox v-if= "questions.length" :currentQuestion = "questions[index]" :next = "next"/></b-col>
+        <b-col><QuestionBox v-if= "questions.length" :currentQuestion = "questions[index]"
+        :next = "next"
+        :increment = "increment"
+        /></b-col>
       </b-row>
     </b-container>
     
@@ -23,12 +29,20 @@ export default {
   data() {
     return {
       questions: [],
-      index: 0
+      index: 0,
+      numCorrect : 0,
+      numTotal: 0
     } 
   },
   methods: {
     next() {
       this.index++
+    },
+    increment(isCorrect){
+      if(isCorrect){
+        this.numCorrect++
+      }
+      this.numTotal++
     }
   },
   mounted: function() {
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,36 +1,114 @@
 <template>
-    <div>
-        <b-jumbotron>
-                <template v-slot:lead>
-                {{currentQuestion.question}}
-                </template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template slot="lead">
+        {{ currentQuestion.question }}
+      </template>
 
-                <hr class="my-4">
+      <hr class="my-4" />
 
-                <b-list-group v-for = "(answer, index) in answers" :key="index">
-                    <b-list-group-item>{{ answer }}</b-list-group-item>
-                </b-list-group>
-                <b-button variant="primary" href="#">Submit</b-button>
-                <b-button @click = "next" variant="success" href="#">Next</b-button>
-        </b-jumbotron>
-    </div>
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in shuffledAnswers"
+          :key="index"
+          @click.prevent="selectAnswer(index)"
+        >
+          {{ answer }}
+        </b-list-group-item>
+      </b-list-group>
+
+      <b-button
+        variant="primary"
+        @click="submitAnswer"
+        :disabled="selectedIndex === null || answered"
+      >
+        Submit
+      </b-button>
+      <b-button @click="next" variant="success">
+        Next
+      </b-button>
+    </b-jumbotron>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            currentQuestion: Object,
-            next: Function
-        },
-        computed: {
-            answers() {
-                let answers = [...this.currentQuestion.incorrect_answers]
-                answers.push(this.currentQuestion.correct_answer)
-                return answers
-            }
-        },
-        mounted() {
-            console.log(this.currentQuestion);
-        }
+import _ from 'lodash'
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function,
+    increment: Function
+  },
+  data: function() {
+    return {
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: [],
+      answered: false
     }
+  },
+  computed: {
+    answers() {
+      // this function is no longer used in finished code
+      // it is replaced by the watch function below and the
+      // shuffleAnswers method
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)
+      return answers
+    }
+  },
+  watch: {
+    currentQuestion: {
+      immediate: true,
+      handler() {
+        this.selectedIndex = null
+        this.answered = false
+        this.shuffleAnswers()
+      }
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index
+    },
+   submitAnswer(){
+     let isCorrect = false
+
+     if(this.selectedIndex === this.correctIndex){
+       isCorrect = true
+     }
+
+     this.answered = true
+
+     this.increment(isCorrect)
+   },
+    shuffleAnswers() {
+      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+      this.shuffledAnswers = _.shuffle(answers)
+      this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+    },
+  }
+}
 </script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.list-group-item:hover {
+  background: #EEE;
+  cursor: pointer;
+}
+.btn {
+  margin: 0 5px;
+}
+.selected {
+  background-color: lightblue;
+}
+.correct {
+  background-color: lightgreen;
+}
+.incorrect {
+  background-color: red;
+}
+</style>
